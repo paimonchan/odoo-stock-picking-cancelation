86e0ccd89346120d6062576f520710bd399736f5
Paimon <nazzunhanif@gmail.com>
2021-10-17 22:06:43 +0700

    initial commit module

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Stock Picking Cancelation",
+    "summary": "Force cancel doned stock picking",
+    "version": "15.0.0",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "author": "Nazzun",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "depends": ["stock_account", "stock"],
+    "data": [
+    ],
+    "installable": True,
+}
