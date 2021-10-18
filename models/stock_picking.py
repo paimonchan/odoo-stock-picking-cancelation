b0157dce882dd0dab45577995f162e7cc5595a70
Paimon <n.h.shounen@gmail.com>
2021-10-18 08:36:37 +0700

    refactor separate cancel movement into another function

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -7,9 +7,11 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     def action_cancel(self):
-        entries = self.filtered(lambda e: e.state != 'cancel')
-        for entry in entries:
-            moves = entry.move_lines
+        def _cancel_stock_move(record):
+            moves = record.move_lines
             move_lines = moves.mapped('move_line_ids')
             move_lines.write(dict(qty_done=0))
             moves.write(dict(state='cancel'))
+        records = self.filtered(lambda e: e.state != 'cancel')
+        for record in records:
+            _cancel_stock_move(record)
