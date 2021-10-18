1da202efb69806d2954c60e0f798f95424fe733c
Paimon <n.h.shounen@gmail.com>
2021-10-18 08:36:55 +0700

    add function to cancel journal

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -12,6 +12,15 @@ class StockPicking(models.Model):
             move_lines = moves.mapped('move_line_ids')
             move_lines.write(dict(qty_done=0))
             moves.write(dict(state='cancel'))
+
+        def _cancel_account_move(record):
+            moves = record.move_lines
+            account_moves = self.env['account.move'].search([
+                ('stock_move_id', 'in', moves.ids)
+            ])
+            account_moves.button_cancel()
+
         records = self.filtered(lambda e: e.state != 'cancel')
         for record in records:
             _cancel_stock_move(record)
+            _cancel_account_move(record)
